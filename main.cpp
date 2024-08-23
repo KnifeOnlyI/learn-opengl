74e238f63c99c6790f6bb238c918c81fbad2276c
Dany Pignoux <dany.pignoux@outlook.fr>
2024-08-23 22:56:01 +0200

    Add SDL2

--- main.cpp
+++ main.cpp
@@ -1,6 +1,14 @@
 #include <iostream>
+#include <SDL2/SDL.h>
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    SDL_Init(SDL_INIT_VIDEO);
+    SDL_Window *window = SDL_CreateWindow("OpenGL", 100, 100, 800, 600, SDL_WINDOW_OPENGL);
+
+    SDL_Delay(1000);
+
+    SDL_DestroyWindow(window);
+
+    SDL_Quit();
     return 0;
 }
